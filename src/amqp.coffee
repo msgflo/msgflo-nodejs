msgflo/msgflo-nodejs

commit d9e70c91ddd87cc4e4541f29996100f6d43ab370

transports: Remove used of deprecated sendToQueue()

src/amqp.coffee
@@ -146,7 +146,7 @@ class Client extends interfaces.MessagingClient
       payload: part
     @channel.assertQueue 'fbp'
     data = new Buffer JSON.stringify msg
-    @channel.sendToQueue 'fbp', data
+    @channel.sendTo 'inqueue', 'fbp', data
     return callback null
 
 class MessageBroker extends Client

src/mqtt.coffee
@@ -106,7 +106,7 @@ class Client extends interfaces.MessagingClient
       protocol: 'discovery'
       command: 'participant'
       payload: part
-    @sendToQueue 'fbp', msg, callback
+    @sendTo 'inqueue', 'fbp', msg, callback
 
 class MessageBroker extends Client
   constructor: (address, options) ->

commit 58f9fa71de20a6b69dea82ab1e5a965a214d20ae

AMQP: Fix exception in registerParticipant()
Regression introduced in d9e70c91dd

src/amqp.coffee
@@ -146,7 +146,7 @@ class Client extends interfaces.MessagingClient
       payload: part
     @channel.assertQueue 'fbp'
     data = new Buffer JSON.stringify msg
-    @channel.sendTo 'inqueue', 'fbp', data
+    @channel.sendToQueue 'fbp', data
     return callback null
 
 class MessageBroker extends Client